e7097f3a3360cbb6c9b8517a3fa5319945d583f5
luishdsg <65797541+luishdsg@users.noreply.github.com>
2025-12-30 20:29:51 -0300

    update delete

--- frontend/src/shared/enum/employees-gender.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum EmployeesGender {
-    MALE = 'male',
-    FEMALE = 'female',
-  }
--- frontend/src/utils/cpfFormatation.ts
+++
@@ -1,6 +0,0 @@
-export const formatCPF = (cpf: string) => {
-    if (cpf.length === 11) {
-      return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
-    }
-    return 'error';
-  };
